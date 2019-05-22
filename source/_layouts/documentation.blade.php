40518c259333116d0e444866a83f82ff44545b6b
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-05-22 09:18:13 +0700

    styling max width

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -11,7 +11,7 @@
             @include('_nav.menu', ['items' => $page->navigation])
         </nav>
 
-        <div class="markdown w-full break-words pb-16 lg:pl-4" v-pre>
+        <div class="documentation markdown w-full break-words pb-16 lg:pl-4" v-pre>
             @yield('content')
         </div>
     </div>
