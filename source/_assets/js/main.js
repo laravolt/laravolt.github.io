453791bf941fdb35db6b8c868b395ef29a767830
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-02-21 11:41:59 +0700

    add `diff` highlight

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -8,6 +8,7 @@ hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
 hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
 hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'));
 
 document.querySelectorAll('pre code').forEach((block) => {
     hljs.highlightBlock(block);
