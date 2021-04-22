b87867358d4d1a3b2f5a5213870674c5bf69e67d
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-04-22 10:17:17 +0700

    certification: set lang to "en"

--- source/certification.blade.php
+++ source/certification.blade.php
@@ -4,7 +4,7 @@
     <section class="container max-w-5xl mx-auto px-6 md:px-8 py-4">
         <iframe
                 title="Sertifikasi Laravolt"
-                src="https://sertiva.id/embed-verification"
+                src="https://sertiva.id/embed-verification?lang=en"
                 frameborder="0"
                 width="100%"
                 height="800px">
